620f09af26de16137038b3abd4eaa516d5c3b93a
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2025-07-06 21:18:28 +0300

    feat: add Type definitions

---
+++ visualization/app/types/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @file index.ts
+ * @brief Type definitions export barrel
+ * @author unoRadar Project
+ * @date 2025
+ */
+
+export type {
+  RadarDataPoint,
+  SerialStatus,
+  SerialConfig,
+  SweepDirection,
+  RadarState,
+} from './radar';
